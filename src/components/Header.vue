be2cf41c3e8ec5207d23a9054a1c9000d2146561
Sara Gisondi <sara.gisondi@outlook.it>
2023-05-02 20:33:39 +0200

    add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,9 +14,16 @@ import {store} from '../data/store';
 
 <template>
   <header>
-    <!-- <img src="" alt="logo"> -->
-    <h2>logo</h2>
-    <h1>{{store.HeaderTitle}}</h1>
+
+    
+    <div class="container-img">
+      <img src="../../public/Yugioh_anime_logo.webp" alt="logo">
+    </div>
+
+    <div class="container">
+      <div class="title">{{store.HeaderTitle}}</div>
+    </div>
+
   </header>
 </template>
 
@@ -24,5 +31,30 @@ import {store} from '../data/store';
 <style lang="scss" scoped>
   header{
     display: flex;
+    align-items: center;
+    height:73px;
+
+
+    .container-img{
+      width: 10%;
+      height:100%;
+      vertical-align: middle;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img{
+        width: 60%;
+        vertical-align: middle;
+      }
+    }
+
+    .container{
+      .title{
+        font-size: xxx-large;
+        font-weight:bold;
+      }
+
+    }
   }
 </style>
